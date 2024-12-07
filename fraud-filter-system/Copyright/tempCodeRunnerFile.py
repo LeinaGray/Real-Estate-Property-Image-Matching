c962f1fc175dec69a1e28c3130940503e052722f
Leina Gray <abad.alleina@gmail.com>
2024-12-07 12:47:16 +0800

    - removed other fraud filter files

--- fraud-filter-system/Copyright/tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-print('\nPicture from Google')
-get_exif_data('C:/Users/lenovo/OneDrive/Documents/PROJECTS/ALT/Real-Estate-Image-Matching/pictures/Google.jpg')
-print('\nPicture from Camera')
-get_exif_data('C:/Users/lenovo/OneDrive/Documents/PROJECTS/ALT/Real-Estate-Image-Matching/pictures/Camera.jpg')
--- fraud-filter-system/Copyright/watermark-detection/tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-from transformers import pipeline
-
-object_detector = pipeline(task = "object-detection", model="rnud/detr-resnet50-finetuning-watermark-pita")
-objects = object_detector("data-scraping/watermarked_images/watermarked_image_0.jpg")
-print(len(objects))
-for obj in objects:
-    print(obj)
